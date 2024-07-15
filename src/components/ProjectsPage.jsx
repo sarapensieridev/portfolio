888f2618b9d754464df6e12dcfac5967137319c3
Sara Pensieri <sarapensieri@gmail.com>
2024-07-15 17:08:04 +0200

    add moenia gallery projects page

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
+import close from "./close.png";
+import { Carousel } from './Carousel';
+import carouselData from '../data/carouselData.json';
+
+const {slides} = carouselData;
 
 const ProjectsPage = () => {
+
+	const navigate = useNavigate();
+		const closeWindow = () => {
+		navigate(-1);
+		}
 	return (
-		<div className="projects-page">Hello World!</div>
+		<div className="projects-page">
+			<button onClick={closeWindow} className="close-button"><img src={close} className="close" alt="close-tag"></img></button>
+			<div className='moenia-pres'>
+				<h2 className='moenia-title'>Moenia landing page</h2>
+				<Carousel data= {slides} />
+			</div>
+		</div>
 	)
 }
 
