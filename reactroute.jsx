3e251622db9b70da739337cd331ea15437015ed2
Sara Pensieri <sarapensieri@gmail.com>
2024-08-13 13:02:38 +0200

    homepage responsive

---
+++ reactroute.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+import { HashRouter } from "react-router-dom";
+import Home from './components/Home';
+
+function App() {
+  return (
+  <HashRouter>
+      <div className="app-main" id="app">
+        <Home />
+        </div>
+    </HashRouter>
+  );
+}
+
+export default App;
+
+
+
+
+
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Projects from './Projects';
+import About from './About';
+import Contacts from './Contacts';
+import Resume from './Resume';
+import Logo from './Logo';
+
+const Home = () => {
+   return (
+   	<div className="app-main" id="components">
+   		<Routes>
+   			<Route path="logo" element={<Logo />} />
+      		<Route path="/about" element={<About />} />
+      		<Route path="/resume" element={<Resume />} />
+      		<Route path="/contacts" element={<Contacts />} />
+      		<Route path="/projects" element={<Projects />} />
+      	</Routes>
+    </div>
+      )
+  }
+
+  export default Home;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
 import Projects from './Projects';
 import About from './About';
 import Contacts from './Contacts';
 import Resume from './Resume';
 import Logo from './Logo';
-import AboutPage from './AboutPage';
-import ResumePage from './ResumePage';
-import ContactsPage from './ContactsPage';
-import ProjectsPage from './ProjectsPage';
+
 
 
 const Home = () => {
