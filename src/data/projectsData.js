d6ef82d0bbff7826af49dd5e6422afaff832d94b
sarapensieridev <sarapensieri@gmail.com>
2025-11-19 10:46:16 +0100

    solved: colors in ProjectPage

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,3 +1,4 @@
+import { pink } from "../constants";
 import imagesData from "./imagesData.json";
 
 const { slides } = imagesData;
@@ -30,15 +31,15 @@ const projectsData = [
       </>,
       <>
         The app's frontend was built with{" "}
-        <b style={{ color: "#da4167" }}>Angular</b>. It uses Angular's
+        <b style={{ color: pink }}>Angular</b>. It uses Angular's
         components and routing to create an organized and easy-to-navigate
         interface. Data is managed through Angular services and state management
-        utilizing <b style={{ color: "#da4167" }}>RxJS observables</b> for
+        utilizing <b style={{ color: pink }}>RxJS observables</b> for
         asynchronous operations. While the current setup uses a local{" "}
-        <b style={{ color: "#da4167" }}>JSON Server</b> for development, its
+        <b style={{ color: pink }}>JSON Server</b> for development, its
         service-oriented design allows for easy integration with a full-fledged
         backend. The app's style is achieved with{" "}
-        <b style={{ color: "#da4167" }}>plain CSS</b> and it is designed to be
+        <b style={{ color: pink }}>plain CSS</b> and it is designed to be
         fully responsive, adapting its layout and presentation across various
         devices to provide an optimal user experience.
       </>,
@@ -70,8 +71,8 @@ const projectsData = [
         daily life, with instant feedback. 
         <br />
       The app's was built with{" "}
-        <b style={{ color: "#da4167" }}>React Native</b> and {" "}
-        <b style={{ color: "#da4167" }}>Expo</b> framework.
+        <b style={{ color: pink }}>React Native</b> and {" "}
+        <b style={{ color: pink }}>Expo</b> framework.
       </>,
     ],
   },
@@ -85,9 +86,9 @@ const projectsData = [
         Unpredictable weather can quickly derail travel plans. Nube Viajera
         solves this problem by providing real-time weather alerts and smart
         recommendations within Spain. Built with{" "}
-        <b style={{ color: "#da4167" }}>React</b>, the app uses geolocation and
+        <b style={{ color: pink }}>React</b>, the app uses geolocation and
         communicates with a trusted, free, and official weather API via a
-        dedicated <b style={{ color: "#da4167" }}>Node.js</b> backend to keep
+        dedicated <b style={{ color: pink }}>Node.js</b> backend to keep
         users informed about current and upcoming conditions.
       </>,
       <>
@@ -97,7 +98,7 @@ const projectsData = [
         warnings to indicate alert severity, provide clear visual cues about the
         nature of the weather warnings. The app features a responsive design and
         a toggleable dark/light mode for optimal usability.{" "}
-        <b style={{ color: "#da4167" }}>Styled Components</b> were used for
+        <b style={{ color: pink }}>Styled Components</b> were used for
         styling.
       </>,
     ],
@@ -107,25 +108,26 @@ const projectsData = [
     title: "LinguaSync Full Stack Project",
     imagesData: linguasync,
     paragraphs: [
-      'LinguaSync is a language learning platform that empowers users to connect with a network of language teachers. Users can browse a wide selection of teachers, filtering by language and viewing detailed profiles. Once registered, users can book appointments, manage their schedules, and securely pay for sessions through our integrated <b style={{ color: "#da4167" }}>Stripe</b> payment system.',
       <>
+      LinguaSync is a language learning platform that empowers users to connect with a network of language teachers. Users can browse a wide selection of teachers, filtering by language and viewing detailed profiles. Once registered, users can book appointments, manage their schedules, and securely pay for sessions through our integrated <b style={{ color: pink }}>Stripe</b> payment system.
+      
         The frontend was constructed with{" "}
-        <b style={{ color: "#da4167" }}>React.js</b>.{" "}
-        <b style={{ color: "#da4167" }}>React Router</b> facilitates seamless
+        <b style={{ color: pink }}>React.js</b>.{" "}
+        <b style={{ color: pink }}>React Router</b> facilitates seamless
         navigation, and a modular architecture promotes code reusability.{" "}
-        <b style={{ color: "#da4167" }}>Tailwind CSS</b> was selected to enable
+        <b style={{ color: pink }}>Tailwind CSS</b> was selected to enable
         rapid development and consistent styling. The backend is built using{" "}
-        <b style={{ color: "#da4167" }}>Node.js</b> and the{" "}
-        <b style={{ color: "#da4167" }}>Express.js</b> framework. RESTful APIs
+        <b style={{ color: pink }}>Node.js</b> and the{" "}
+        <b style={{ color: pink }}>Express.js</b> framework. RESTful APIs
         enable efficient data exchange between the frontend and backend,
         adhering to API design best practices.{" "}
-        <b style={{ color: "#da4167" }}>Axios</b> is employed for asynchronous
+        <b style={{ color: pink }}>Axios</b> is employed for asynchronous
         API fetching in the React.js frontend. Data persistence is managed using{" "}
-        <b style={{ color: "#da4167" }}>MongoDB</b>. The endpoints were tested
-        using <b style={{ color: "#da4167" }}>Postman</b>. Authentication is
+        <b style={{ color: pink }}>MongoDB</b>. The endpoints were tested
+        using <b style={{ color: pink }}>Postman</b>. Authentication is
         handled via a secure, token-based system, supporting multi-level access
         control for users, teachers, and administrators.{" "}
-        <b style={{ color: "#da4167" }}>Stripe</b> integration leverages its API
+        <b style={{ color: pink }}>Stripe</b> integration leverages its API
         for secure and reliable online payment processing.
       </>,
     ],
@@ -137,8 +139,8 @@ const projectsData = [
     paragraphs: [
       <>
         This project showcases the development of a robust and user-friendly
-        admin dashboard utilizing <b style={{ color: "#da4167" }}>React</b> and{" "}
-        <b style={{ color: "#da4167" }}>SCSS</b> and it was designed
+        admin dashboard utilizing <b style={{ color: pink }}>React</b> and{" "}
+        <b style={{ color: pink }}>SCSS</b> and it was designed
         specifically for an eLearning platform, English Seed. By leveraging the
         power of React's component-based architecture, the dashboard seamlessly
         integrates interactive elements, data visualizations, and a
@@ -147,7 +149,7 @@ const projectsData = [
       </>,
       <>
         This frontend project incorporates React Query for data handling and
-        testing. Testing with <b style={{ color: "#da4167" }}>React Query</b>{" "}
+        testing. Testing with <b style={{ color: pink }}>React Query</b>{" "}
         was conducted on key data-driven pages, including Users, Orders, and
         Products management sections. This project not only addresses a crucial
         need in education but also serves as a valuable template for developing
@@ -173,16 +175,16 @@ const projectsData = [
       <>
         Plantastic is an e-commerce app built from scratch, designed to allow
         users to browse and purchase various types of plants. The app utilizes{" "}
-        <b style={{ color: "#da4167" }}>React</b> for the frontend,{" "}
-        <b style={{ color: "#da4167" }}>Node.js</b> for the backend,{" "}
-        <b style={{ color: "#da4167" }}>MongoDB</b> as the database and styling
-        with <b style={{ color: "#da4167" }}>Styled Components</b>.
+        <b style={{ color: pink }}>React</b> for the frontend,{" "}
+        <b style={{ color: pink }}>Node.js</b> for the backend,{" "}
+        <b style={{ color: pink }}>MongoDB</b> as the database and styling
+        with <b style={{ color: pink }}>Styled Components</b>.
       </>,
       <>
         Key features include secure user authentication via{" "}
-        <b style={{ color: "#da4167" }}>JWT</b>, global state management with{" "}
-        <b style={{ color: "#da4167" }}>Redux</b>, and payment integration with{" "}
-        <b style={{ color: "#da4167" }}>Stripe</b>. The frontend is built using
+        <b style={{ color: pink }}>JWT</b>, global state management with{" "}
+        <b style={{ color: pink }}>Redux</b>, and payment integration with{" "}
+        <b style={{ color: pink }}>Stripe</b>. The frontend is built using
         React, following the component-based architecture. Redux is used to
         manage the state across the app, including the user's cart,
         authentication status, and the payment process. The backend is built
@@ -198,8 +200,11 @@ const projectsData = [
     title: "Unwind Stays Angular App",
     imagesData: unwind,
     paragraphs: [
-      'Unwind Stays is a web application designed to facilitate the discovery and rental of holiday homes in natural European settings. Built with the latest version of <b style={{ color: "#da4167" }}>Angular</b>, it offers a seamless user experience.',
-      "The application leverages the latest version of Angular, employing standalone components for a modular and maintainable codebase. This architecture ensures a responsive and efficient user experience. Styling is achieved through standard CSS, focusing on clarity and usability. Unwind Stays provides intuitive search and a filtering option that allows users to quickly pinpoint their ideal location. Furthermore, Unwind Stays is designed to work seamlessly on all devices, ensuring a responsive experience.",
+      <>
+      'Unwind Stays is a web application designed to facilitate the discovery and rental of holiday homes in natural European settings. Built with the latest version of <b style={{ color: pink }}>Angular</b>, it offers a seamless user experience.
+      <br />
+      The application leverages the latest version of Angular, employing standalone components for a modular and maintainable codebase. This architecture ensures a responsive and efficient user experience. Styling is achieved through standard CSS, focusing on clarity and usability. Unwind Stays provides intuitive search and a filtering option that allows users to quickly pinpoint their ideal location. Furthermore, Unwind Stays is designed to work seamlessly on all devices, ensuring a responsive experience.
+      </>
     ],
   },
   {
@@ -210,11 +215,12 @@ const projectsData = [
       <>
         A landing page was developed to introduce Moenia, an app dedicated to
         exploring Europe's archaeological treasures. The page was designed in{" "}
-        <b style={{ color: "#da4167" }}>React</b> to captivate users, inform
+        <b style={{ color: pink }}>React</b> to captivate users, inform
         them about Moenia's features, and drive app downloads.
-      </>,
-      "By adhering to Moenia's established design language, a visually appealing and intuitive user interface was created. The landing page highlights key functionalities through interactive elements, making the app's value proposition clear and engaging. A multi-language navbar ensures accessibility for a global audience.",
-      "The landing page was designed with a clear and concise structure to optimize user experience and encourage app downloads. Prominent calls-to-action guide visitors towards the download page, while contact and social media information is provided for additional engagement.",
+        <br />
+      By adhering to Moenia's established design language, a visually appealing and intuitive user interface was created. The landing page highlights key functionalities through interactive elements, making the app's value proposition clear and engaging. A multi-language navbar ensures accessibility for a global audience.
+      The landing page was designed with a clear and concise structure to optimize user experience and encourage app downloads. Prominent calls-to-action guide visitors towards the download page, while contact and social media information is provided for additional engagement.
+    </>
     ],
   },
 ];
